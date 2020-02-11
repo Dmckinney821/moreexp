94123329c4e8b6ed0d55c9cb716eb2b4cc75d2c9
Dan McKinney <3dhdaniel@gmail.com>
2020-02-11 10:23:04 -0500

    maybe

---
+++ public/scripts/app.js
@@ -0,0 +1,80 @@
+'use strict';
+
+console.log('App js is running');
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'This is some info',
+    options: ['One', 'Two']
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+var user = {
+    name: 'Dan',
+    age: 63
+};
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+// function getAge(age) {
+//     if(age >= 18) {
+//         return <p>Age: {age}</p>
+//     }
+// }
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
