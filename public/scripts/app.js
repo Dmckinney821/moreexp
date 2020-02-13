5bd97946be16a4a83c6c7c13e8092e2bb8cd0a73
Dan McKinney <3dhdaniel@gmail.com>
2020-02-13 15:17:47 -0500

    thursday

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,88 @@
 'use strict';
 
-// const add = (a, b) => {
-//     console.log(arguments)
-//     return a + b;
+console.log('App js is running');
+
+// const app = {
+//     title: 'Indecision App',
+//     subtitle: 'This is some info',
+//     options: []
 // }
+var app = {
+    title: 'Visibility Toggle',
+    details: 'Hey. These are some details you can see now'
+    // const onFormSubmit = (e) => {
+    //     e.preventDefault();
 
-// console.log(add(2,3,10))
+    //     const option = e.target.elements.option.value;
+    //     console.log(option)
+    //     if(option){
+    //         app.options.push(option)
+    //         e.target.elements.option.value = ''
+    //     }
+    //     render()
+    // }
+    // const whipArray = (option) => {
+    //     app.options = []
+    //     render()
+    // }
+    // const onMakeDecision = () => {
+    //     const randomNum = Math.floor(Math.random() * app.options.length);
+    //     const option = app.options[randomNum];
+    //     alert(option)
+    //     console.log(randomNum)
+    // }
+};var appRoot = document.getElementById('app');
+// const numbers = [55, 101, 1000]
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>{app.title}</h1>
+//             {app.subtitle && <p>{app.subtitle}</p>}
+//             <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+//             <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do</button>
+//             <button onClick={whipArray}>Remove All</button>
+//             <ol>
+//             {
+//                 app.options.map((option) => <li key={option}>Option: {option}</li>)
+//             }
+//             </ol>
+//             <form onSubmit={onFormSubmit}> 
+//                 <input type='text' name='option'/>
+//                 <button>Add options</button>
 
-var user = {
-    name: 'Mickey',
-    cities: ['Atl', 'LA', 'SF'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
-    }
+//             </form>
+//         </div>
+//     )
+//     ReactDOM.render(template, appRoot)
+// }
+// render()
+var showDeets = function showDeets() {
+    visibility = !visibility;
+    render();
 };
-console.log(user.printPlacesLived());
+var visibility = false;
+var render = function render() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            app.title
+        ),
+        React.createElement('br', null),
+        React.createElement(
+            'button',
+            { onClick: showDeets },
+            visibility ? 'Hide Details' : 'Show Details'
+        ),
+        visibility && React.createElement(
+            'p',
+            null,
+            app.details
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+render();
