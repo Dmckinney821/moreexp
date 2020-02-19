dbc2f233c1693ffc41fb576ed1868a64d5252ad6
Dan McKinney <3dhdaniel@gmail.com>
2020-02-19 10:49:40 -0500

    wacky wednesday; g

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,106 +1,92 @@
 'use strict';
 
-var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var VisibilityToggle = function (_React$Component) {
+    _inherits(VisibilityToggle, _React$Component);
 
-var Person = function () {
-    function Person() {
-        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    function VisibilityToggle(props) {
+        _classCallCheck(this, VisibilityToggle);
 
-        _classCallCheck(this, Person);
+        var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
 
-        this.name = name, this.age = age;
-    }
-
-    _createClass(Person, [{
-        key: 'getGreeting',
-        value: function getGreeting() {
-            // return 'Hi I am ' + this.name + '!'
-            return 'Hi I am ' + this.name + '!';
-        }
-    }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            return this.name + ' am ' + this.age + ' years old.';
-        }
-    }]);
-
-    return Person;
-}();
-
-var Student = function (_Person) {
-    _inherits(Student, _Person);
-
-    function Student(name, age, major) {
-        _classCallCheck(this, Student);
-
-        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
-
-        _this.major = major;
+        _this.handToggleVisibility = _this.handToggleVisibility.bind(_this);
+        _this.state = {
+            visibility: false
+        };
         return _this;
     }
 
-    _createClass(Student, [{
-        key: 'hasMajor',
-        value: function hasMajor() {
-            return !!this.major;
+    _createClass(VisibilityToggle, [{
+        key: 'handToggleVisibility',
+        value: function handToggleVisibility() {
+            this.setState(function (prevState) {
+                return {
+                    visibility: !prevState.visibility
+                };
+            });
         }
     }, {
-        key: 'getDescription',
-        value: function getDescription() {
-            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
-            if (this.hasMajor()) {
-                description += ' Their major is ' + this.major;
-            }
-            return description;
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'h1',
+                    null,
+                    'Visibility Toggle'
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: this.handToggleVisibility },
+                    this.state.visibility ? 'Hide details' : 'Show details'
+                ),
+                this.state.visibility && React.createElement(
+                    'div',
+                    null,
+                    React.createElement(
+                        'p',
+                        null,
+                        'Hey. These are some details'
+                    )
+                )
+            );
         }
     }]);
 
-    return Student;
-}(Person);
+    return VisibilityToggle;
+}(React.Component);
 
-var Traveler = function (_Person2) {
-    _inherits(Traveler, _Person2);
+ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById('app'));
 
-    function Traveler(name, age, homeLocation) {
-        _classCallCheck(this, Traveler);
+// let visibility = false;
 
-        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     render()
+// }
 
-        _this2.homeLocation = homeLocation;
-        return _this2;
-    }
-
-    _createClass(Traveler, [{
-        key: 'hasLocation',
-        value: function hasLocation() {
-            return !!this.homeLocation;
-        }
-    }, {
-        key: 'getGreeting',
-        value: function getGreeting() {
-            var location = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
-            if (this.hasLocation()) {
-                location += ' I am visiting from ' + this.homeLocation;
-            }
-            return location;
-        }
-    }]);
-
-    return Traveler;
-}(Person);
-
-var me = new Traveler('Mickey', 36, 'Ocala');
-
-console.log(me.getGreeting());
-
-var other = new Traveler();
-console.log(other.getGreeting());
+// const render = () => {
+//     const jsx = (
+//         <div>   
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleVisibility}>
+//                 {visibility ? 'Hide details' : 'Show details'}
+//             </button>
+//             {visibility && (
+//                 <div>
+//                 <p>Hey. These are some details</p>
+//                 </div>
+//             )}
+//         </div>
+//     )
+//     ReactDOM.render(jsx, document.getElementById('app'))
+// }
+// render()
