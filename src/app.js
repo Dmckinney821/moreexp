7994a4b80fca61d391e0b7d41b3315a9f554b98a
Dan McKinney <3dhdaniel@gmail.com>
2020-02-25 10:44:51 -0500

    practice

--- src/app.js
+++ src/app.js
@@ -4,28 +4,4 @@ import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
 
 
-ReactDOM.render((<IndecisionApp />), document.getElementById('app'))
-
-class OldSyntax {
-    constructor() {
-        this.name = 'Dan';
-        this.getGreeting = this.getGreeting.bind(this)
-    }
-    getGreeting() {
-        return `Hi, myname is ${this.name}`;
-    }
-}
-const oldSyntax = new OldSyntax()
-const getGreeting = oldSyntax.getGreeting
-console.log(getGreeting())
-
-// -------
-class NewSyntax {
-    name = 'Dan';
-    getGreeting = () => {
-        return `Hi my name is ${this.name}`
-    }
-}
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newGetGreeting())
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,25 +3,36 @@ import AddOption from './AddOption'
 import Options from './Options'
 import Header from './Header'
 import Action from './Action'
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
     state = {
-        options: []
+        options: [],
+        selectedOption: undefined
     }
     
     handleDeleteOptions = () => {
         this.setState(() => ({ options: [] }))
     }
+
     handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({ 
             options: prevState.options.filter((option) => optionToRemove !== option)
         }))
     }
 
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ 
+            selectedOption: undefined
+        }))
+    }
+
     handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
-        alert(option)
+        this.setState({
+            selectedOption: option
+        })
     }
     handleAddOption = (option) => {
         if(!option) {
@@ -74,6 +85,10 @@ export default class IndecisionApp extends React.Component {
                 <AddOption 
                     handleAddOption={this.handleAddOption}
                 />
+                <OptionModal 
+                    selectedOption={this.state.selectedOption}
+                    handleClearSelectedOption={this.handleClearSelectedOption}
+                    />
             </div>
         )
     }
