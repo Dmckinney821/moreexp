c45543566375301900fc2ad7e0f31867bc5555ce
Dan McKinney <3dhdaniel@gmail.com>
2020-03-04 10:19:30 -0500

    wednesday

--- src/app.js
+++ src/app.js
@@ -3,7 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import { setTextFilter } from './actions/filters';
+import { addExpense } from './actions/expenses'
+import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss'
 
+const store = configureStore();
+store.dispatch(addExpense({ description: 'Water bill'}))
+store.dispatch(addExpense({ description: 'Gas bill'}))
+store.dispatch(setTextFilter('Water'))
 
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+
+console.log(store.getState())
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
