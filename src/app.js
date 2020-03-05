398ca8a6d3ff1985a47f1abb923dad06d6a66906
Dan McKinney <3dhdaniel@gmail.com>
2020-03-05 12:05:18 -0500

    wednesday;

--- src/app.js
+++ src/app.js
@@ -11,19 +11,14 @@ import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss'
 
 const store = configureStore();
+
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500}))
-store.dispatch(addExpense({ description: 'Gas bill'}))
-store.dispatch(setTextFilter('Water'))
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}))
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}))
 
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('rent'))
-}, 3000)
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
-
-console.log(store.getState())
 
 const jsx = (
     <Provider store={store}>
