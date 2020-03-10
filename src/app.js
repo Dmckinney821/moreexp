f310ca5e4ce673e71fd75cbaa92913a60c4c2349
Dan McKinney <3dhdaniel@gmail.com>
2020-03-10 10:29:35 -0400

    working hard for the money

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { setTextFilter } from './actions/filters';
 import { addExpense } from './actions/expenses'
 import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
 
